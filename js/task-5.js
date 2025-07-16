221a7d35e34565d0d44bad82a6381756be558db9
notzuki <101867510+notzuki@users.noreply.github.com>
2025-07-16 20:15:59 +0300

    task 5 finished

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnChangeColor = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+btnChangeColor.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  spanColor.textContent = newColor;
+});
