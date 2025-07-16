84c8b01118c153a6270f72fc1133bf865a90a2dc
notzuki <101867510+notzuki@users.noreply.github.com>
2025-07-16 20:47:24 +0300

    стиль для галереї

--- js/task-2.js
+++ js/task-2.js
@@ -25,7 +25,7 @@ const images = [
   }
 ];
 
-const galleryEl = document.querySelector('.gallery');
+const fullGallery = document.querySelector('.gallery');
 
 const galleryElItems = images
   .map(
@@ -33,8 +33,8 @@ const galleryElItems = images
   <li class="gallery-item">
     <img class="gallery-image" src="${image.url}" alt="${image.alt}" width="360">
   </li>
-`
+  `
   )
   .join('');
 
-galleryEl.insertAdjacentHTML('beforeend', galleryElItems);
+fullGallery.insertAdjacentHTML('beforeend', galleryElItems);
