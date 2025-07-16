0b28827798bc8d35a6299ae372f036a7883ba6d9
notzuki <101867510+notzuki@users.noreply.github.com>
2025-07-16 20:19:09 +0300

    task 4 finished

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,26 @@
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const { email, password } = e.currentTarget.elements;
+
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (!emailValue || !passwordValue) {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+  e.currentTarget.reset();
+}
 
